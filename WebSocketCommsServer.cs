ac6a028ad913875fb807e72b586aa7aa484d8b24
ChrisB <chris.barnard@btinternet.com>
2018-10-01 09:17:00 +0100

    Alow CommsServer to shut down gracefully

--- WebSocketCommsServer.cs
+++ WebSocketCommsServer.cs
@@ -16,7 +16,7 @@ using WebSocketSharp.Server;
 
 namespace WebSocketComms
 {
-	public class CommsServer
+	public class CommsServer : IDisposable
 	{
 		object _commandStructure;
 		string _linkName;
@@ -79,7 +79,7 @@ namespace WebSocketComms
 			}
 		}
 
-		public class WebSocketListener : WebSocketBehavior
+		public class WebSocketListener : WebSocketBehavior, IDisposable
 		{
 			object _commandStructure;
 			CommsServer _server;
@@ -205,7 +205,43 @@ namespace WebSocketComms
                 Send(sw.ToString());
                 sw.Dispose();*/
 			}
-		}
+
+            #region IDisposable Support
+            private bool disposedValue = false; // To detect redundant calls
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!disposedValue)
+                {
+                    if (disposing)
+                    {
+                        running = false;
+                        sendSignal.Set();
+                    }
+
+                    // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                    // TODO: set large fields to null.
+
+                    disposedValue = true;
+                }
+            }
+
+            // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+            // ~WebSocketListener() {
+            //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            //   Dispose(false);
+            // }
+
+            // This code added to correctly implement the disposable pattern.
+            public void Dispose()
+            {
+                // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+                Dispose(true);
+                // TODO: uncomment the following line if the finalizer is overridden above.
+                // GC.SuppressFinalize(this);
+            }
+            #endregion
+        }
 
 		public void ShowMethods(Type type)
 		{
@@ -298,5 +334,48 @@ namespace WebSocketComms
 				}
 			}
 		}
-	}
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if(_wssv != null)
+                    {
+                        foreach (var l in webSocketListeners)
+                        {
+                            l.Context.WebSocket.Close();
+                            l.Dispose();
+                        }
+                        _wssv.Stop();
+                    }
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~CommsServer() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
 }
